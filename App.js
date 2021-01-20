9206b3d60b369f8285a91f411bc1febaa4f00bdc
Michael Lock <thelockymichael@gmail.com>
2021-01-20 16:18:04 +0200

    added working eslint and prettier

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
-import {StatusBar} from 'expo-status-bar';
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Jotain lol="Fasjfoi" assgags="asfaga" asgjadngk="422412" />
     </View>
   );
 }
+
+const Jotain = (props) => (
+  <View>
+    <Text>{props.lol}</Text>
+  </View>
+);
 
 const styles = StyleSheet.create({
   container: {
