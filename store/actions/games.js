898878d2481a1732194ca3a11069c8590ae0b676
Michael Lock <thelockymichael@gmail.com>
2021-01-27 21:01:07 +0200

    added FlatList data for multiscore draws option

--- store/actions/games.js
+++ store/actions/games.js
@@ -24,7 +24,7 @@ export const getDraws = (gameType) => {
 
     const resData = await response.json()
 
-    // console.log("resData", resData.draws);
+    console.log("resData", resData);
 
 
     dispatch(
--- views/DrawsScreen.js
+++ views/DrawsScreen.js
@@ -9,11 +9,14 @@ import {
   FlatList
 } from 'react-native';
 
+import moment from "moment"
+
 import { useDispatch } from 'react-redux'
 import { useSelector } from "react-redux"
 import * as gamesActions from '../store/actions/games'
 
-import SelectMultipleGroupButton from "../components/UI/SelectMultipleGroupButton"
+import SelectMultipleGroupButton
+  from "../components/UI/SelectMultipleGroupButton"
 
 // Colors
 import Colors from "../constants/Colors"
@@ -26,6 +29,25 @@ const DrawsScreen = ({ navigation }) => {
   console.log(draws);
 
   const dispatch = useDispatch()
+
+  const itemsExample = [
+    {
+      id: 1,
+      content: "Hello"
+    },
+    {
+      id: 2,
+      content: "Hello"
+    },
+    {
+      id: 3,
+      content: "Hello"
+    },
+    {
+      id: 4,
+      content: "Hello"
+    },
+  ]
 
   console.log(selectedDraw);
   return (
@@ -57,8 +79,25 @@ const DrawsScreen = ({ navigation }) => {
         />
       </View>
       <Text>DrawsScreen</Text>
+      <Text>DrawsScreen</Text>
       <FlatList
+        data={draws}
+        renderItem={itemData => (
+          <View>
+            <Text>{itemData.item.status}</Text>
+            <Text>{itemData.item.status}</Text>
+            <Text>Open time: {moment(itemData.item.openTime).format("DD MMM YYYY hh:mm a")}</Text>
+            <Text>Close time: {moment(itemData.item.closeTime).format("DD MMM YYYY hh:mm a")}</Text>
+            <Text>Results available time: {moment(itemData.item.resultsAvailableTime).format("DD MMM YYYY hh:mm a")}</Text>
+            <Text>{"==========ROWS========="}</Text>
+            {itemData.item.rows.map(item => (
+              <Text>{item.name}</Text>
+            ))}
 
+            <Text>{"==========================="}</Text>
+
+          </View>
+        )}
       />
     </View>
   );
